e649c6834a57876a6c2da9329332142c22be42d0
NatxoFerro <natxoferro@gmail.com>
2021-07-23 17:35:55 +0200

    OrderNotFoundException message fix

--- src/main/java/com/example/OrderNotFoundException.java
+++ src/main/java/com/example/OrderNotFoundException.java
@@ -3,7 +3,7 @@ package com.example;
 public class OrderNotFoundException extends RuntimeException {
 
 	OrderNotFoundException(Long id) {
-		super("Employee with ID " + id + " not found!");
+		super("Order with ID " + id + " not found!");
 	}
 
 }
